0774ec6aae2710264edffe3e82962b9188311edd
Northug <47028633+Northug@users.noreply.github.com>
2021-01-10 23:07:04 +0600

    2306

--- src/main/resources/db/migration/V3__Add_admin.sql
+++ src/main/resources/db/migration/V3__Add_admin.sql
@@ -2,12 +2,21 @@ create extension if not exists pgcrypto;
 
 INSERT INTO public.usr(
 	id, created, email, email_confirmed, password, username)
-	VALUES (1, current_date, 'admin@admin.com', true, crypt('admin', gen_salt('bf',10)), 'sharestory5january');
+	VALUES (1, current_date, 'sharestory.projectt@gmail.com', true, crypt('sharestory5january', gen_salt('bf',10)), 'admin');
 	
 INSERT INTO public.user_roles(
 	user_id, role_id)
 	VALUES (1, 1),
 	(1,3);
 	
-ALTER SEQUENCE usr_id_seq restart with 2;
+	INSERT INTO public.usr(
+	id, created, email, email_confirmed, password, username)
+	VALUES (2, current_date, 'serejasmail@mail.ru', true, crypt('realnando5', gen_salt('bf',10)), 'realnando');
+	
+INSERT INTO public.user_roles(
+	user_id, role_id)
+	VALUES (2, 1),
+	(2,3);
+	
+ALTER SEQUENCE usr_id_seq restart with 3;
 	
