05ee0f2f97ae8a9dee46dd1a931dc2e2ad1e1895
Northug <47028633+Northug@users.noreply.github.com>
2021-01-10 20:10:01 +0600

    2009

--- src/main/resources/db/migration/V4__Add_one_more_admin.sql
+++ src/main/resources/db/migration/V4__Add_one_more_admin.sql
@@ -2,9 +2,9 @@ create extension if not exists pgcrypto;
 
 INSERT INTO public.usr(
 	id, created, email, email_confirmed, password, username)
-	VALUES (6, current_date, 'admin2@admin.com', true, crypt('sharestory5january', gen_salt('bf',10)), 'admin');
+	VALUES (10, current_date, 'admin2@admin2.com', true, crypt('sharestory5january', gen_salt('bf',10)), 'admin2');
 	
 INSERT INTO public.user_roles(
 	user_id, role_id)
-	VALUES (6, 1),
-	(6,3);
+	VALUES (10, 1),
+	(10,3);
